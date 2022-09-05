5bdaf340c7b17f364fc51a89a869dc0709066d31
eljc <elder.jc@hotmail.com>
2022-09-05 15:51:31 -0300

    Application

--- food-store-api/src/main/java/com/eljc/qik/FoodStoreApplication.java
+++ food-store-api/src/main/java/com/eljc/qik/FoodStoreApplication.java
@@ -2,8 +2,10 @@ package com.eljc.qik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class FoodStoreApplication {
 
 	public static void main(String[] args) {
